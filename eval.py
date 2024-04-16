6d66f05914d43c4bda5a7e10e1e68e5d22686a24
SZucchini <suzuki.tomohiro@g.sp.m.is.nagoya-u.ac.jp>
2024-04-16 14:51:38 +0900

    hhcl feature extraction

--- eval.py
+++ eval.py
@@ -4,15 +4,19 @@ import random
 
 import numpy as np
 import torch
+import torch.nn as nn
 import torch.nn.utils.rnn as rnn
+from PIL import Image
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
 import hhcl
 from configs.defaults import get_cfg_defaults
 from dataset.dataset import RunnerDataset
+from hhcl import datasets
 from hhcl.utils.data import transforms as T
 from models.gruae import GRUAutoEncoder
+from train_hhcl import get_test_loader as get_shoes_loader
 
 
 def get_cfg(config):
@@ -20,6 +24,12 @@ def get_cfg(config):
     cfg.merge_from_file(config)
     cfg.freeze()
     return cfg
+
+
+def get_data(name, data_dir):
+    root = data_dir
+    dataset = datasets.create(name, root)
+    return dataset
 
 
 def set_seed(seed):
@@ -86,28 +96,56 @@ def main():
         normalizer
     ])
     hhcl_dataset = RunnerDataset(root_dir, transform=test_transformer)
+    shoes_dataset = get_data("custom", f"./data/HHCL/shoes/{args.type}")
 
-    gruae = GRUAutoEncoder(cfg)
-    gruae.load_state_dict(torch.load(f"./output/gruae/{args.type}/checkpoint/last_dict.pth"))
-    gruae.to(cfg.MODEL.DEVICE)
-    gruae.eval()
+    # gruae = GRUAutoEncoder(cfg)
+    # gruae.load_state_dict(torch.load(f"./output/gruae/{args.type}/checkpoint/last_dict.pth"))
+    # gruae.to(cfg.MODEL.DEVICE)
+    # gruae.eval()
 
-    hhclp = torch.load(f"./output/hhcl/persons/{args.type}/final_model.pth")
-    # hhcls = torch.load("./output/hhcl/shoes/{}/final_model.pth")
+    # gru_features = []  # (Query, Dim)
+    # for x in test_loader:
+    #     x = x.to(cfg.MODEL.DEVICE)
+    #     with torch.no_grad():
+    #         _, hidden = gruae(x, 0)
+    #         feature = hidden.squeeze(0).cpu().numpy()
+    #         if len(gru_features) == 0:
+    #             gru_features.append(feature)
+    #             gru_features = np.array(gru_features).squeeze()
+    #         else:
+    #             gru_features = np.concatenate([gru_features, feature], axis=0)
 
-    gru_features = []
-    for x in test_loader:
-        x = x.to(cfg.MODEL.DEVICE)
-        with torch.no_grad():
-            _, hidden = gruae(x, 0)
-            feature = hidden.squeeze(0).cpu().numpy()
-            if len(gru_features) == 0:
-                gru_features.append(feature)
-                gru_features = np.array(gru_features).squeeze()
-            else:
-                gru_features = np.concatenate([gru_features, feature], axis=0)
-    print(gru_features.shape)
-    del gruae
+    # del gruae
+    # hhclp = torch.load(f"./output/hhcl/persons/{args.type}/final_model.pth")
+    # hhclp.to(cfg.MODEL.DEVICE)
+    # hhclp.eval()
+
+    # hhclp_features = []
+    # for i in range(len(hhcl_dataset)):
+    #     x, _ = hhcl_dataset[i]
+    #     x = x.to(cfg.MODEL.DEVICE)
+    #     with torch.no_grad():
+    #         feature = hhclp(x).cpu().numpy()
+    #         feature = np.mean(feature, axis=0)
+    #         hhclp_features.append(feature)
+    # hhclp_features = np.array(hhclp_features)
+
+    # del hhclp, hhcl_dataset
+    # hhcls = torch.load(f"./output/hhcl/shoes/{args.type}/final_model.pth")
+    # hhcls.to(cfg.MODEL.DEVICE)
+    # hhcls.eval()
+
+    # hhcls_features = []
+    # for i in range(len(shoes_dataset.query)):
+    #     shoe_img_path, _, _ = shoes_dataset.query[i]
+    #     shoe_img = Image.open(shoe_img_path)
+    #     x = test_transformer(shoe_img).unsqueeze(0).to(cfg.MODEL.DEVICE)
+    #     with torch.no_grad():
+    #         feature = hhcls(x).squeeze(0).cpu().numpy()
+    #         hhcls_features.append(feature)
+    # hhcls_features = np.array(hhcls_features)
+
+    # del hhcls
 
 
 if __name__ == "__main__":
